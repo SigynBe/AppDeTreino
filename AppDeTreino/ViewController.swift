fa22c90a9376c3fc3721720e950f36ee33fb6219
Rebeca PachecoCorrea <rebecapc@icomp.ufam.edu.br>
2020-08-12 09:41:39 -0400

    Initial Commit

---
+++ AppDeTreino/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppDeTreino
+//
+//  Created by Rebeca Pacheco on 12/08/20.
+//  Copyright © 2020 Rebeca Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
